7d481761ac3da05130fbf609eb0fad2e7a570af2
Francisco  Estudillo <franciscoestudillo97@outlook.com>
2025-09-01 11:45:36 -0600

    archivos adicionales

--- lib/screens/ingresos_screen.dart
+++ lib/screens/ingresos_screen.dart
@@ -1,4 +1,4 @@
-import 'package:app_control_kinder_v4/models/alumno.dart';
+// import 'package:app_control_kinder_v4/models/alumno.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/pago.dart';
